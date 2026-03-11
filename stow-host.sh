aa7cbf1d9528f3780ba54f706d3f435996b25e1f
Paul Baumann <p.baumann@list-info.de>
2026-03-11 19:21:25 +0100

    .

---
+++ stow-host.sh
@@ -0,0 +1,24 @@
+
+#!/usr/bin/env bash
+
+set -e
+shopt -s nullglob
+
+HOST_DIR=host-${1:-$HOSTNAME}
+
+[[ -d "$HOST_DIR" ]] || {
+    echo "Host directory not found: $HOST_DIR" 1>&2
+    exit 1
+}
+
+shift || true
+
+for dir in common "$HOST_DIR"; do
+    find "$dir" -mindepth 1 -maxdepth 1 -type d | read || continue
+    pushd "$dir" &>/dev/null
+    echo "Stowing $dir files..."
+    [[ "$#" -gt 0 ]] || set -- *
+    stow -t "$HOME" "$@"
+    popd &>/dev/null
+done
+ 
